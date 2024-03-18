1bc97f7364e4599b704a80e6f852193a66a96aac
deepak maurya <114849725+deepakkrmaurya@users.noreply.github.com>
2024-03-18 13:58:46 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose'
 
 const dbCOnnection = async()=>{
     try {
-        const connection = await mongoose.connect(process.env.MONGOOSE_URI)
+        const connection = await mongoose.connect(process.env.MONGOOSE_URI || 'mongodb+srv://deepak:deepak123@cluster0.rafupcb.mongodb.net/erp')
         if(connection){
             console.log("DB Connection Successfully...")
         }
